fc3514a50b5e1d0c8f7a6e6239fc0d045e4088df
codeofhassan <theultimatehassan@gmail.com>
2024-10-30 16:18:26 +0500

    Created MVC for Expense

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// RoutE::get('/',function(){
+//     return view('expenses.index');
+// });
+
+Route::get('/', function() {
+    return redirect()->route('expenses.index');
 });
+
+Route::resource('expenses', ExpenseController::class);
